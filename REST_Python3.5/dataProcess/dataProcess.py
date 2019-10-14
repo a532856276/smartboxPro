4b967770df4f66aaa99f59c4715a42054383f110
a532856276 <532856276@qq.com>
2019-10-14 20:40:40 +0800

    损失最大

--- REST_Python3.5/dataProcess/dataProcess.py
+++ REST_Python3.5/dataProcess/dataProcess.py
@@ -17,22 +17,27 @@ import copy
 g_HighQueue = Queue()
 g_LowQueue = Queue()
 KlineQueue = Queue()
-g_KlineUpDwnSquence = 0
+g_KlineUpDwnSequence = 0
 maxLine = []
 lowLine = []
 g_LastKline = {}
 g_LastTwoKline = 0
-g_KlineSave =[]
-g_WillDoing= 0
+g_KlineSave = []
+g_WillDoing = 0
+
+
 def doNothing():
     pass
+
+
 def ProcessKline(noDo, willDo):
     if willDo in [myEnum.RISING_HAS_FALLING, myEnum.RISING_IN_FALLING, myEnum.RISING_TO_FALLING]:
         g_OrderSellOpenEvent.set()
-    elif willDo in[myEnum.FALLING_HAS_RISING, myEnum.FALLING_IN_RISING, myEnum.FALLING_HAS_RISING]:
+    elif willDo in [myEnum.FALLING_HAS_RISING, myEnum.FALLING_IN_RISING, myEnum.FALLING_HAS_RISING]:
         g_OrderBuyOpenEvent.set()
     else:
         print("ProcessKline Error")
+
 
 # 相当于 getUpDwnSquence
 def dataDecodeFromNet(contractKLineLst):
@@ -41,53 +46,55 @@ def dataDecodeFromNet(contractKLineLst):
     # 最新数据在list最后
     global maxLine
     global lowLine
-    global g_KlineUpDwnSquence
+    global g_KlineUpDwnSequence
     global g_LastKline
     global g_WillDoing
-    global g_halfOfLastKline
+    #global g_halfOfLastKline
 
     currentKline = 0
     lastKline = 0
 
     if len(maxLine) or len(lowLine):
-        lastKline = g_KlineUpDwnSquence & 0x1
+        lastKline = g_KlineUpDwnSequence & 0x1
         lastKlinePos = len(g_KlineSave) - 1
-        idex = len(contractKLineLst) - 1 - 1
-        
-        g_halfOfLastKline = getHalfOfKline(contractKLineLst[idex])
+        index = len(contractKLineLst) - 1 - 1
 
-        currentKline = getUpOrDwn(contractKLineLst[idex])
-        g_KlineUpDwnSquence = g_KlineUpDwnSquence << 1 | currentKline
-        tempKline = copy.deepcopy(contractKLineLst[idex])
+        halfOfLastKline = getHalfOfKline(contractKLineLst[index])
+        setHalf(halfOfLastKline)
 
-        #nowStatus = ishammer(tempKline)
-        nextDoing = getType(g_KlineSave[lastKlinePos],contractKLineLst[idex])
-        #ProcessKline(nowStatus, nextDoing)
+        currentKline = getUpOrDwn(contractKLineLst[index])
+        g_KlineUpDwnSequence = g_KlineUpDwnSequence << 1 | currentKline
+        tempKline = copy.deepcopy(contractKLineLst[index])
+
+        # nowStatus = ishammer(tempKline)
+        nextDoing = getType(g_KlineSave[lastKlinePos], contractKLineLst[index])
+        # ProcessKline(nowStatus, nextDoing)
 
         if nextDoing in [myEnum.RISING_HAS_FALLING, myEnum.RISING_IN_FALLING, myEnum.RISING_TO_FALLING]:
             g_OrderSellOpenEvent.set()
             logger.info(u'开空 by kLine:{0}:{1},nextDoing:{2}'.format(lastKline, currentKline, nextDoing))
-        elif nextDoing in[myEnum.FALLING_HAS_RISING, myEnum.FALLING_IN_RISING, myEnum.FALLING_HAS_RISING]:
+        elif nextDoing in [myEnum.FALLING_HAS_RISING, myEnum.FALLING_IN_RISING, myEnum.FALLING_HAS_RISING]:
             g_OrderBuyOpenEvent.set()
             logger.info(u'开多 by kLine:{0}:{1},nextDoing:{2}'.format(lastKline, currentKline, nextDoing))
-        elif nextDoing in [myEnum.RISING_ONLY_DWN,myEnum.RISING_EQ_DWN,myEnum.RISING_EQ_EQ,myEnum.RISING_EQ_UP,myEnum.RISING_HAMMER_UP,myEnum.RISING_HAMMER_DWN,myEnum.RISING_ONLY_DWN] :
-            if currentKline == 1: # and nextDoing == myEnum.RISING_ONLY_DWN:
+        elif nextDoing in [myEnum.RISING_ONLY_DWN, myEnum.RISING_EQ_DWN, myEnum.RISING_EQ_EQ, myEnum.RISING_EQ_UP,
+                           myEnum.RISING_HAMMER_UP, myEnum.RISING_HAMMER_DWN, myEnum.RISING_ONLY_DWN]:
+            if currentKline == 1:  # and nextDoing == myEnum.RISING_ONLY_DWN:
                 g_OrderSellOpenEvent.set()
                 logger.info(u'开空 by kLine:{0}:{1},nextDoing'.format(lastKline, currentKline))
             else:
                 g_OrderBuyOpenEvent.set()
                 logger.info(u'开多 by kLine:{0}:{1}'.format(lastKline, currentKline))
-        #if lastKline > currentKline: # 阳阴
+        # if lastKline > currentKline: # 阳阴
         #    if nextDoing in [myEnum.RISING_HAS_FALLING, myEnum.RISING_IN_FALLING, myEnum.RISING_TO_FALLING]:
         #        g_OrderSellOpenEvent.set()
         #        logger.info(u'开空 by kLine:{0}:{1},nextDoing:{2}'.format(lastKline, currentKline, nextDoing))
         #    maxLine.append(tempKline)
-        #elif lastKline < currentKline: # 阴阳
+        # elif lastKline < currentKline: # 阴阳
         #    if nextDoing in[myEnum.FALLING_HAS_RISING, myEnum.FALLING_IN_RISING, myEnum.FALLING_HAS_RISING]:
         #        g_OrderBuyOpenEvent.set()
         #        logger.info(u'开多 by kLine:{0}:{1},nextDoing:{2}'.format(lastKline, currentKline, nextDoing))
         #    lowLine.append(tempKline)
-        #else: # ͬ保持不变
+        # else: # ͬ保持不变
         #    if nowStatus in [myEnum.RISING_ONLY_DWN,myEnum.RISING_EQ_DWN,myEnum.RISING_EQ_EQ,myEnum.RISING_EQ_UP,myEnum.RISING_HAMMER_UP,myEnum.RISING_HAMMER_DWN,myEnum.RISING_ONLY_DWN] :
         #        if currentKline == 1:
         #            g_OrderSellOpenEvent.set()
@@ -96,86 +103,99 @@ def dataDecodeFromNet(contractKLineLst):
         #            g_OrderBuyOpenEvent.set()
         #            logger.info(u'开多 by kLine:{0}:{1},nowStatus:{2}'.format(lastKline, currentKline, nowStatus))
 
-        g_LastKline = copy.deepcopy(contractKLineLst[idex])
+        g_LastKline = copy.deepcopy(contractKLineLst[index])
         g_KlineSave.append(g_LastKline)
-        
-        #getLastState()
+
+        # getLastState()
     else:
-        g_KlineUpDwnSquence = 0
+        g_KlineUpDwnSequence = 0
         logger.info(u' kLine:{0}'.format(contractKLineLst))
-        for idex in range(len(contractKLineLst) - 1):
-            currentKline = getUpOrDwn(contractKLineLst[idex])
-            g_KlineUpDwnSquence = g_KlineUpDwnSquence << 1 | currentKline
-            tempKline = copy.deepcopy(contractKLineLst[idex])
-            if lastKline > currentKline: # 阳阴
+        for index in range(len(contractKLineLst) - 1):
+            currentKline = getUpOrDwn(contractKLineLst[index])
+            g_KlineUpDwnSequence = g_KlineUpDwnSequence << 1 | currentKline
+            tempKline = copy.deepcopy(contractKLineLst[index])
+            if lastKline > currentKline:  # 阳阴
                 maxLine.append(tempKline)
-            elif lastKline < currentKline: # 阴阳
+            elif lastKline < currentKline:  # 阴阳
                 lowLine.append(tempKline)
-            else: # ͬ保持不变
+            else:  # ͬ保持不变
                 pass
             lastKline = currentKline
             g_LastKline.clear()
-            g_LastKline = copy.deepcopy(contractKLineLst[idex])
-            g_halfOfLastKline = getHalfOfKline(contractKLineLst[idex])
+            g_LastKline = copy.deepcopy(contractKLineLst[index])
+            halfOfLastKline = getHalfOfKline(contractKLineLst[index])
+            setHalf(halfOfLastKline)
             g_KlineSave.append(tempKline)
 
+
 def getLastState():
-    lastTwoKline = g_KlineUpDwnSquence & 0x3
+    lastTwoKline = g_KlineUpDwnSequence & 0x3
     lenAll = len(g_KlineSave)
-    is_Hammer = ishammer(g_KlineSave[lenAll-1])
+    is_Hammer = ishammer(g_KlineSave[lenAll - 1])
     if lastTwoKline == 0 or lastTwoKline == 3:
-        lastThrKline = g_KlineUpDwnSquence & 0x7
-        if is_Hammer is myEnum.RISING_EQ_UP :
+        lastThrKline = g_KlineUpDwnSequence & 0x7
+        if is_Hammer is myEnum.RISING_EQ_UP:
             if lastTwoKline == 0:
-                g_OrderBuyOpenEvent.set() #开多
+                g_OrderBuyOpenEvent.set()  # 开多
                 logger.info(u'开多 by kLine:{0},{1}'.format(lastTwoKline, is_Hammer))
                 if lastThrKline == 0:
-                    g_OrderBuyOpenEvent.set() #开多
+                    g_OrderBuyOpenEvent.set()  # 开多
                     logger.info(u'开多 by kLine:{0},{1}'.format(lastTwoKline, is_Hammer))
         elif is_Hammer is myEnum.RISING_EQ_EQ:
             pass
         elif is_Hammer is myEnum.RISING_EQ_DWN:
             if lastTwoKline == 3:
-                g_OrderSellOpenEvent.set() #开空
+                g_OrderSellOpenEvent.set()  # 开空
                 logger.info(u'开空 by kLine:{0},{1}'.format(lastTwoKline, is_Hammer))
                 if lastThrKline == 7:
-                    g_OrderBuyOpenEvent.set() #开多
+                    g_OrderBuyOpenEvent.set()  # 开多
                     logger.info(u'开多 by kLine:{0},{1}'.format(lastTwoKline, is_Hammer))
-        
-    else: # 2
+
+    else:  # 2
         if is_Hammer is None:
             pass
-        elif is_Hammer is myEnum.RISING_ONLY_DWN.value or is_Hammer is myEnum.RISING_ONLY_UP.value\
-          or is_Hammer is myEnum.RISING_HAMMER_DWN.value or is_Hammer is myEnum.RISING_HAMMER_UP.value:
+        elif is_Hammer is myEnum.RISING_ONLY_DWN.value or is_Hammer is myEnum.RISING_ONLY_UP.value \
+                or is_Hammer is myEnum.RISING_HAMMER_DWN.value or is_Hammer is myEnum.RISING_HAMMER_UP.value:
             if lastTwoKline == 1:
-                g_OrderBuyOpenEvent.set() #开多
+                g_OrderBuyOpenEvent.set()  # 开多
                 logger.info(u'开多 by kLine:{0},{1}'.format(lastTwoKline, is_Hammer))
             else:
-                g_OrderSellOpenEvent.set() #开空
+                g_OrderSellOpenEvent.set()  # 开空
                 logger.info(u'开空 by kLine:{0},{1}'.format(lastTwoKline, is_Hammer))
 
 
 def testdataDecodeFromNet():
-    global g_KlineUpDwnSquence
+    global g_KlineUpDwnSequence
     global g_PrintFlag
     g_PrintFlag = False
-    inputdata = [\
-        {'amount': 17137.364187577074, 'close': 314.737, 'count': 3028, 'high': 317.259, 'id': 1569121200, 'low': 314.126, 'open': 317.144, 'vol': 540888}, \
-        {'amount': 17796.991253268166, 'close': 315.697, 'count': 2957, 'high': 316.081, 'id': 1569124800, 'low': 313.673, 'open': 314.738, 'vol': 560246}, \
-        {'amount': 13152.020335745643, 'close': 317.092, 'count': 2361, 'high': 318.04, 'id': 1569128400, 'low': 315.174, 'open': 315.71, 'vol': 416880}, \
-        {'amount': 8614.527755853285, 'close': 315.642, 'count': 1422, 'high': 317.356, 'id': 1569132000, 'low': 315.091, 'open': 317.031, 'vol': 272368}, \
-        {'amount': 12063.814470932364, 'close': 317.906, 'count': 1993, 'high': 318.482, 'id': 1569135600, 'low': 315.461, 'open': 315.508, 'vol': 383192}]
-    g_KlineUpDwnSquence = g_KlineUpDwnSquence
+    inputdata = [ \
+        {'amount': 17137.364187577074, 'close': 314.737, 'count': 3028, 'high': 317.259, 'id': 1569121200,
+         'low': 314.126, 'open': 317.144, 'vol': 540888}, \
+        {'amount': 17796.991253268166, 'close': 315.697, 'count': 2957, 'high': 316.081, 'id': 1569124800,
+         'low': 313.673, 'open': 314.738, 'vol': 560246}, \
+        {'amount': 13152.020335745643, 'close': 317.092, 'count': 2361, 'high': 318.04, 'id': 1569128400,
+         'low': 315.174, 'open': 315.71, 'vol': 416880}, \
+        {'amount': 8614.527755853285, 'close': 315.642, 'count': 1422, 'high': 317.356, 'id': 1569132000,
+         'low': 315.091, 'open': 317.031, 'vol': 272368}, \
+        {'amount': 12063.814470932364, 'close': 317.906, 'count': 1993, 'high': 318.482, 'id': 1569135600,
+         'low': 315.461, 'open': 315.508, 'vol': 383192}]
+    g_KlineUpDwnSequence = g_KlineUpDwnSequence
     dataDecodeFromNet(inputdata)
 
-    testCase("g_KlineUpDwnSquence by dataDecodeFromNet",g_KlineUpDwnSquence ,0x6)
+    testCase("g_KlineUpDwnSquence by dataDecodeFromNet", g_KlineUpDwnSequence, 0x6)
 
-    inputdata = [\
-        {'amount': 17137.364187577074, 'close': 314.737, 'count': 3028, 'high': 317.259, 'id': 1569121200, 'low': 314.126, 'open': 317.144, 'vol': 540888}, \
-        {'amount': 17796.991253268166, 'close': 315.697, 'count': 2957, 'high': 316.081, 'id': 1569124800, 'low': 313.673, 'open': 314.738, 'vol': 560246}, \
-        {'amount': 13152.020335745643, 'close': 317.092, 'count': 2361, 'high': 318.04, 'id': 1569128400, 'low': 315.174, 'open': 315.71, 'vol': 416880}, \
-        {'amount': 8614.527755853285, 'close': 315.642, 'count': 1422, 'high': 317.356, 'id': 1569132000, 'low': 315.091, 'open': 317.031, 'vol': 272368}, \
-        {'amount': 12063.814470932364, 'close': 317.906, 'count': 1993, 'high': 318.482, 'id': 1569135600, 'low': 315.461, 'open': 315.508, 'vol': 383192},\
-        {'amount': 12063.814470932364, 'close': 317.906, 'count': 1993, 'high': 318.482, 'id': 1569135600, 'low': 315.461, 'open': 317.906, 'vol': 383192}]
+    inputdata = [ \
+        {'amount': 17137.364187577074, 'close': 314.737, 'count': 3028, 'high': 317.259, 'id': 1569121200,
+         'low': 314.126, 'open': 317.144, 'vol': 540888}, \
+        {'amount': 17796.991253268166, 'close': 315.697, 'count': 2957, 'high': 316.081, 'id': 1569124800,
+         'low': 313.673, 'open': 314.738, 'vol': 560246}, \
+        {'amount': 13152.020335745643, 'close': 317.092, 'count': 2361, 'high': 318.04, 'id': 1569128400,
+         'low': 315.174, 'open': 315.71, 'vol': 416880}, \
+        {'amount': 8614.527755853285, 'close': 315.642, 'count': 1422, 'high': 317.356, 'id': 1569132000,
+         'low': 315.091, 'open': 317.031, 'vol': 272368}, \
+        {'amount': 12063.814470932364, 'close': 317.906, 'count': 1993, 'high': 318.482, 'id': 1569135600,
+         'low': 315.461, 'open': 315.508, 'vol': 383192}, \
+        {'amount': 12063.814470932364, 'close': 317.906, 'count': 1993, 'high': 318.482, 'id': 1569135600,
+         'low': 315.461, 'open': 317.906, 'vol': 383192}]
     dataDecodeFromNet(inputdata)
-    testCase("g_KlineUpDwnSquence by dataDecodeFromNet",g_KlineUpDwnSquence ,0xD)
+    testCase("g_KlineUpDwnSquence by dataDecodeFromNet", g_KlineUpDwnSequence, 0xD)
--- REST_Python3.5/main.py
+++ REST_Python3.5/main.py
@@ -11,8 +11,10 @@ TestBign = False
 
 g_BeginPrice = 5
 
+
 def TestCase():
     testdataDecodeFromNet()
+
 
 def ContractOrderInfoNew():
     res = 0
@@ -21,37 +23,36 @@ def ContractOrderInfoNew():
         print(u'ContractOrderInfoNew 查询订单失败 {0}'.format(contractPositionInfo['msg']))
         time.sleep(1)
         return False
-    #print(u'ContractOrderInfoNew 查询订单: {0}'.format(contractPositionInfo))
-    global g_BuyMaxProfit,g_SellMaxProfit
+    # print(u'ContractOrderInfoNew 查询订单: {0}'.format(contractPositionInfo))
+    global g_BuyMaxProfit, g_SellMaxProfit
 
     if len(contractPositionInfo["data"]) == 0:
         print(u'ContractOrderInfoNew 查询订单无数据')
         g_BuyMaxProfit = 0.0
         g_SellMaxProfit = 0.0
         return False
-    contractBuyInfoFlag= 0
+    contractBuyInfoFlag = 0
     contractSellInfoFlag = 0
     for value in contractPositionInfo["data"]:
         # print(value)
         if value['direction'] == "buy":
             orderPriceThreadLock.acquire()  # 获取线程价格锁
             g_ContractBuyInfo.setValue(value['volume'], value['available'], value['cost_open'], value['profit'])
-            g_OrderBuyOpenPrice = value['cost_open']
             orderPriceThreadLock.release()  # 释放线程锁
             contractBuyInfoFlag = 1
             buyProfit = value['profit']
             if buyProfit < 0.0:
                 g_BuyMaxProfit = 0
                 if buyProfit <= FIXCLOSEPROFIT:
-                    g_OrderSellCloseEvent.set()  #平多
+                    g_OrderSellCloseEvent.set()  # 平多
             else:
                 if g_BuyMaxProfit == 0.0:
                     g_BuyMaxProfit = buyProfit
                 else:
                     if buyProfit > g_BuyMaxProfit:
                         g_BuyMaxProfit = buyProfit
-                    elif buyProfit <= g_BuyMaxProfit * 0.92 and buyProfit >= g_BuyMaxProfit * 0.5:
-                        g_OrderSellCloseEvent.set()  #平多
+                    elif g_BuyMaxProfit * 0.92 >= buyProfit >= g_BuyMaxProfit * 0.5:
+                        g_OrderSellCloseEvent.set()  # 平多
                         logger.info(u'多单利润 -- 需要平多')
             logger.info(u'多单利润A({0}/({1}))'.format(buyProfit, g_BuyMaxProfit))
         if value['direction'] == "sell":
@@ -61,19 +62,19 @@ def ContractOrderInfoNew():
             orderPriceThreadLock.release()  # 释放线程锁
             contractSellInfoFlag = 1
             sellProfit = value['profit']
-            
+
             if sellProfit < 0.0:
                 g_SellMaxProfit = 0
                 if sellProfit <= FIXCLOSEPROFIT:
-                    g_OrderBuyCloseEvent.set()  #平空
+                    g_OrderBuyCloseEvent.set()  # 平空
             else:
                 if g_SellMaxProfit == 0:
                     g_SellMaxProfit = sellProfit
                 else:
                     if sellProfit > g_SellMaxProfit:
                         g_SellMaxProfit = sellProfit
-                    elif sellProfit <= g_SellMaxProfit * 0.92 and sellProfit >= g_SellMaxProfit * 0.5:
-                        g_OrderBuyCloseEvent.set()  #平空
+                    elif g_SellMaxProfit * 0.92 >= sellProfit >= g_SellMaxProfit * 0.5:
+                        g_OrderBuyCloseEvent.set()  # 平空
                         logger.info(u'空单利润---({0}/{1}) 减小需要平空'.format(sellProfit, g_SellMaxProfit))
                     else:
                         pass
@@ -82,26 +83,27 @@ def ContractOrderInfoNew():
         orderPriceThreadLock.acquire()  # 获取线程价格锁
         # logger.info(u'多单利润({0})'.format(0))
         g_ContractBuyInfo.setValue()
-        g_OrderBuyOpenPrice = 0
         orderPriceThreadLock.release()  # 释放线程锁
+        g_BuyMaxProfit = 0
 
     if contractSellInfoFlag == 0:
         orderPriceThreadLock.acquire()  # 获取线程价格锁
         # logger.info(u'空单利润({0})'.format(0))
         g_ContractSellInfo.setValue()
-        g_OrderSellOpenPrice = 0
         orderPriceThreadLock.release()  # 释放线程锁
+        g_SellMaxProfit = 0
     return True
+
 
 def GetContraInfoTreadNew(args, kwargs):
     while True:
-        #print("GetContraInfoTreadNew...")
+        # print("GetContraInfoTreadNew...")
         res = ContractOrderInfoNew()
         if res is True:
             time.sleep(1)
         else:
             time.sleep(3)
-        
+
 
 def CreatOrderThread():
     print("CreatOrderThread")
@@ -120,22 +122,25 @@ def CreatOrderThread():
     # 使用说明：
     # 在需要查询处 执行 g_GetContraInfoEvent.set, 使用 g_ContractSellInfo 获取 空单信息，g_ContractBuyInfo 获取多单信息
     # GetContraInfoTread = threading.Thread(target=GetContraInfoTread, args={"GetContraInfoTread", 'GetContraInfoTread111'})
-    GetContraInfoTread = threading.Thread(target=GetContraInfoTreadNew, args={"GetContraInfoTread", 'GetContraInfoTread111'})
+    GetContraInfoTread = threading.Thread(target=GetContraInfoTreadNew,
+                                          args={"GetContraInfoTread", 'GetContraInfoTread111'})
     GetContraInfoTread.start()
+
 
 def PriceProcess(newPrice):
     lenKline = len(g_KlineSave)
-    state = getType(g_KlineSave[lenAll-1], newPrice)
+    state = getType(g_KlineSave[lenAll - 1], newPrice)
     pass
+
 
 def Process(high, open, close, low):
     upValue = high - close
     if upValue == 0 and (high - low) >= 20:
         upFlag = True
-        logger.info(u'上位 by upValue:{0},high({1})close:{2}'.format(upValue,tem['high'], tem['close']))
+        logger.info(u'上位 by upValue:{0},high({1})close:{2}'.format(upValue, tem['high'], tem['close']))
     if upFlag is True and upValue >= 15:
         upFlag = False
-        g_OrderSellCloseEvent.set()  #平多
+        g_OrderSellCloseEvent.set()  # 平多
         g_OrderSellOpenEvent.set()
         logger.info(u'开空 by upValue:{0},close:{1}'.format(upValue, tem['close']))
 
@@ -147,7 +152,7 @@ if __name__ == "__main__":
     else:
         CreatOrderThread()
         g_QueryKline = MyTimer(GetContractKline, 15, True)
-        #启动定时任务，定时时间由入参决定
+        # 启动定时任务，定时时间由入参决定
         g_QueryKline.start();
 
         if GetContractInfo(contractType=g_ContractType):  # this_week(当周) next_week(下周) quarter(季度)
@@ -170,15 +175,15 @@ if __name__ == "__main__":
         sellMaxValue = 0
         GetContractKline()
         while True:
-            runcnt = runcnt + 1 
+            runcnt = runcnt + 1
             print("main process {0}".format(runcnt))
             time.sleep(3)
-            #continue
+            # continue
 
-            #maxLen = len(maxLine)
-            #lowLen = len(lowLine)
-            #marketToOrder = GetContractMarketMerged()
-            #print("{0}".format(marketToOrder))
+            # maxLen = len(maxLine)
+            # lowLen = len(lowLine)
+            # marketToOrder = GetContractMarketMerged()
+            # print("{0}".format(marketToOrder))
             marketToOrder = GetContractKlineTime()
             if marketToOrder['status'] is 'fail':
                 msgErrorCnt = msgErrorCnt + 1
@@ -186,7 +191,7 @@ if __name__ == "__main__":
                     time.sleep(1)
                 print("main process getMsgErrorCnt {0}".format(msgErrorCnt))
                 continue
-            
+
             msgErrorCnt = 0
 
             if marketToOrder is not None and marketToOrder['status'] == "ok":
@@ -194,26 +199,27 @@ if __name__ == "__main__":
                     print("main process data is NULL")
                     continue
 
-                global g_halfOfLastKline
                 tem = marketToOrder['data'][0]
-                
-                
+
+                g_halfOfLastKline = getHalf()
                 if g_halfOfLastKline != 0.0:
                     if tem['open'] > g_halfOfLastKline:
                         if tem['close'] <= g_halfOfLastKline and g_OrderSellOpenFlag is False:
                             g_OrderSellOpenEvent.set()
-                            logger.info(u'开空 by g_halfOfLastKline:{0}>=close:{1}'.format(g_halfOfLastKline, tem['close']))
+                            logger.info(
+                                u'开空 by g_halfOfLastKline:{0}>=close:{1}'.format(g_halfOfLastKline, tem['close']))
                     elif tem['open'] < g_halfOfLastKline:
                         if tem['close'] >= g_halfOfLastKline and g_OrderBuyOpenFlag is False:
                             g_OrderBuyOpenEvent.set()
-                            logger.info(u'开多  by g_halfOfLastKline:{0}<=close:{1}'.format(g_halfOfLastKline, tem['close']))
+                            logger.info(
+                                u'开多  by g_halfOfLastKline:{0}<=close:{1}'.format(g_halfOfLastKline, tem['close']))
 
-                #continue
+                # continue
                 # 此处注意刚开盘时，数据量比较小，怎么判断最高和最低？？
                 upValue = tem['high'] - tem['close']
                 if upValue == 0 and tem['high'] - tem['low'] >= 20:
                     upFlag = True
-                    logger.info(u'上顶 by upValue:{0},high({1})close:{2}'.format(upValue,tem['high'], tem['close']))
+                    logger.info(u'上顶 by upValue:{0},high({1})close:{2}'.format(upValue, tem['high'], tem['close']))
                 if upFlag is True and upValue >= g_BeginPrice:
                     upFlag = False
                     g_OrderSellOpenEvent.set()
@@ -226,19 +232,18 @@ if __name__ == "__main__":
                     dwnFlag = True
                 if dwnFlag is True and dwnValue >= g_BeginPrice:
                     dwnFlag = False
-                    g_OrderBuyOpenEvent.set() # 买入开仓
+                    g_OrderBuyOpenEvent.set()  # 买入开仓
                     logger.info(u'开多 by dwnValue:{0},close:{1}'.format(dwnValue, tem['close']))
                     continue
-                
 
-                #if maxLen != 0 and tem['close'] >= maxLine[maxLen - maxLineIndex]['close'] and maxLen != maxLineIndex:
+                # if maxLen != 0 and tem['close'] >= maxLine[maxLen - maxLineIndex]['close'] and maxLen != maxLineIndex:
                 #    print("Kline will buy: close{0} >= maxLine[{0}]".format(tem['close'], maxLine[maxLen - maxLineIndex]['close']))
                 #    g_OrderBuyOpenEvent.set() # 买入开仓
                 #    maxLineIndex = maxLineIndex + 1
                 #    if maxLineIndex >= maxLen:
                 #        maxLineIndex = maxLen
                 #    continue
-                #if lowLen != 0 and tem['close'] <= lowLine[lowLen - lowLineIndex]['close'] and lowLineIndex != lowLen:
+                # if lowLen != 0 and tem['close'] <= lowLine[lowLen - lowLineIndex]['close'] and lowLineIndex != lowLen:
                 #    print("Kline will Sell: close{0} <= lowLine[{1}] -->lowLen:{2} lowLineIndex:{3}".format(tem['close'], lowLine[lowLen - lowLineIndex]['close'], lowLen, lowLineIndex))
                 #    g_OrderSellOpenEvent.set() # 卖出开仓
                 #    lowLineIndex = lowLineIndex + 1
@@ -246,20 +251,18 @@ if __name__ == "__main__":
                 #        lowLineIndex = lowLen
                 #    continue
 
-                #lenKline = len(g_KlineSave)
-                #if lenKline != 0:
+                # lenKline = len(g_KlineSave)
+                # if lenKline != 0:
                 #    pass
-                    #state = getType(g_KlineSave[lenKline-1], newPrice)
-                    #if state ==0 or state == 3:
-                    #    pass
-                    #else:
-                    #    if state == 1:
-                    #        PriceProcess(positionRes)
+                # state = getType(g_KlineSave[lenKline-1], newPrice)
+                # if state ==0 or state == 3:
+                #    pass
+                # else:
+                #    if state == 1:
+                #        PriceProcess(positionRes)
             else:
                 print(u'main: GetContractMarketMerged is error marketToOrder({0})'.format(marketToOrder))
-                
-            #OrderByMarket(positionRes)
 
-            
-            
-            #time.sleep(3)
+            # OrderByMarket(positionRes)
+
+            # time.sleep(3)
